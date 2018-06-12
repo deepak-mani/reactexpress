7eb6a23ca65b7c2683ccf5dd24a5e764edaae98b
Deepak Mani <deepak.m@skava.com>
2018-06-12 18:35:22 +0530

    'version'

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
+const express = require("express");
+const exp = express();
+
+const createServer = port => {
+  const listening = port;
+  exp.get("/deepak", (req, res) => {
+    res.status(200).json({ deepak: "success" });
+  });
+
+  exp.get("*", (req, res) => {
+    res.status(200).json({ deepak: "others", portlis: listening });
+  });
+
+  const onListen = () => console.log("onListen", listening);
+
+  exp.listen(port, onListen);
+};
+
+// createServer(4321);
+// createServer(1234);
 const app = {
   eh: "gnana"
 };
 
 exports.app = app;
+exports.createServer = createServer;
